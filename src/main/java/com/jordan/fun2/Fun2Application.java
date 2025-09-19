bdcf47695923e0196d2e3f29612f63b8f6975d10
Random Internet Person <112799108+YardenDrori@users.noreply.github.com>
2025-09-19 15:48:57 +0300

    initiall commit

---
+++ src/main/java/com/jordan/fun2/Fun2Application.java
@@ -0,0 +1,13 @@
+package com.jordan.fun2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fun2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fun2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordan/fun2/Fun2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordan.fun2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fun2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
