c00c8e1b01dd2b6a85484e342a2c50bb2e08f942
Random Internet Person <112799108+YardenDrori@users.noreply.github.com>
2025-09-19 17:02:12 +0300

    made prep for stage 2

---
+++ src/main/java/com/jordan/stage2/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.jordan.stage2.repository;
+
+import com.jordan.stage2.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+}
---
+++ src/main/java/com/jordan/stage2/stage2Application.java
@@ -0,0 +1,13 @@
+package com.jordan.stage2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class stage2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(stage2Application.class, args);
+	}
+
+}
