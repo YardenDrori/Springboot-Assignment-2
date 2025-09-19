57875055075638d2bdf6d81715b45a2e04899c97
Random Internet Person <112799108+YardenDrori@users.noreply.github.com>
2025-09-19 16:54:37 +0300

    renamed file from leftovers of the template

--- src/main/java/com/jordan/stage1/Stage1Application.java
+++ src/main/java/com/jordan/stage1/Stage1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Fun2Application {
+public class Stage1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Fun2Application.class, args);
+		SpringApplication.run(Stage1Application.class, args);
 	}
 
 }
